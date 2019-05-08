96cf8014b25cbb20b180664d1e0874e8eebc5f8f
Selma2015 <selma.nb@aol.com>
2019-05-08 14:17:55 -0400

    added isCorrectGuess function that checks if character in word

--- Archive/logic.js
+++ Archive/logic.js
@@ -2,5 +2,29 @@
 //Author: MiaTee 
 
 
-//Creating array to store national parks names
+//Array to store national parks names
 var gameWords = ["yosemite", "yellowstone", "acadia", "shenandoah", "halekala", "sequoia", "canyonlands"];
+
+//function to pick random word from the gameWords Array
+function randomWord(gameWords)
+{
+ 
+ return gameWords[Math.floor(Math.random() * gameWords.length)];
+
+}
+
+randomWord();
+
+
+
+//function to check if character in word 
+function isCorrectGuess (word, letter) 
+
+    {
+       return word.includes(letter);
+    }
+   
+
+isCorrectGuess();
+
+
