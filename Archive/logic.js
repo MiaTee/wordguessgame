100cb8b0c821b07f00da27955bedd20edd1475c3
Selma2015 <selma.nb@aol.com>
2019-05-08 17:13:08 -0400

    created a function that will setup the game with  default options

--- Archive/logic.js
+++ Archive/logic.js
@@ -1,9 +1,13 @@
 //National Parks Word Guess Game
 //Author: MiaTee 
 
-
+//Global Variables:
 //Array to store national parks names
 var gameWords = ["yosemite", "yellowstone", "acadia", "shenandoah", "halekala", "sequoia", "canyonlands"];
+
+
+
+//Utility Functions:
 
 //function to pick random word from the gameWords Array
 function randomWord(gameWords)
@@ -57,4 +61,37 @@ for(var i = 0 ; i < word.length; i++)
 }
 fillBlanks();
 
+//Game Logic Functions
 
+//1)- Function to setup the round that will take one argument the picked word and returns guesses left, wrong guesses and puzzle array
+ function setupRound(word)
+ {
+//     // word; pass word to the function
+//     //guessesLeft: set guesses left to 9
+//     //count wrong guess an array that will store wrong guess , start value is empty
+// //     //puzzleState is an array with underscore values at the start
+//      var wrongGuesses = [];
+// //     var puzzleState = getBlank();
+//      var guessesLeft = 9;
+// //     var word = randomWord();
+//     var puzzleState = puzzleArray;
+    
+    // var obj = {
+    //     word: word,
+    //     guessesLeft: guessesLeft,
+    //     wrongGuesses: wrongGuesses,
+    //     puzzleState = puzzleState
+    // };
+    var gameObj = {
+        guessesLeft: 9,
+        word: word,
+        wrongGuesses:[],
+        puzzleState:[]
+    
+
+
+    };
+
+  return gameObj;
+ }
+ setupRound();
