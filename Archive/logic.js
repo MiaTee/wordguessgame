fe126850b3a20d1488cbfaa2f57e7092fab23cc0
Selma2015 <selma.nb@aol.com>
2019-05-08 12:14:06 -0400

    added array to store national parks names and added picturesof each park in the array

---
+++ Archive/logic.js
@@ -0,0 +1,6 @@
+//National Parks Word Guess Game
+//Author: MiaTee 
+
+
+//Creating array to store national parks names
+var gameWords = ["yosemite", "yellowstone", "acadia", "shenandoah", "halekala", "sequoia", "canyonlands"];
